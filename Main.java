b2640828f7a3619797f8a4bc446d014cc3cf279a
krazlwo <12345esd.me@gmail.com>
2025-09-30 18:22:13 +0000

    p3

--- Main.java
+++ Main.java
@@ -2,22 +2,60 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write your code here
-  }
+   lastFirstN("cream", "butter", 3);
+   stringManip("hello", "dogman");
+   System.out.println(removeStr("mushroom", "ush"));
+   
 
   /** Precondition: s1 and s2 are not null
   * Precondition: 0 <= n <= min(s1.length(), s2.length()
   */
   public static void lastFirstN(String s1, String s2, int n)
   {
+    String output;
+    String firstn;
+    String lastn;
+
+   //ex 
+   //s1 = cream
+   //s2 = butter
+   //n = 3
+   // output should give 4
+
+   firstn = s1.substring(0, n);
+   
+   //last n letters of s2
+   int startPosition;
+   startPosition = s2.length() - n;
+   lastn = s2.substring(startPosition);
   
+
+    output = firstn + lastn;
+    System.out.println(output);
   }
 
   /** Precondition: s1 and s2 are not null
   */
   public static void stringManip(String s1, String s2)
   {
-  
+    String poe;
+    poe = s1.toUpperCase();
+
+    //string s2
+
+    String joe;
+    joe = s2.substring(0, 1);
+    String firstcapletter;
+    String lowercaseletters;
+    String printedlowletters; 
+
+    firstcapletter = joe.toUpperCase();
+    lowercaseletters = s2.substring(1);
+    printedlowletters = lowercaseletters.toLowerCase();
+
+
+    System.out.println(poe);
+    System.out.println(firstcapletter + printedlowletters);
   }
 
   /** Precondition: s1 and s2 are not null
@@ -25,7 +63,28 @@ public class Main
   */
   public static String removeStr(String s1, String s2)
   {
-    String output = null;
+  //  int id;
+    //id = s1.indexOf(s2);
+    //int pie;
+    //pie = s2.length(); (needed to put id in the front)
+
+  //String before;
+  //String after;
+
+    //before = s1.substring(id, pie);
+    //after = s1.substring(pie);
+
+    //String output = before + after;
+    //return output;
+    String output;
+    String before;
+    String after;
+    int startofs2 = s1.indexOf(s2);
+    int endofs2 = startofs2 + s2.length(); 
+
+    before = s1.substring(0, startofs2);
+    after = s1.substring(endofs2);
+    output = before + after;
     return output;
   }
 }
